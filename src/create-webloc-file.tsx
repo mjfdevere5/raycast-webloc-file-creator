855c1ac5c742a680715b544549f4fe3a03497003
Max <8040671+mjfdevere5@users.noreply.github.com>
2025-04-25 14:17:43 +0200

    MVP working.

--- src/create-webloc-file.tsx
+++ src/create-webloc-file.tsx
@@ -1,5 +1,55 @@
-import { Detail } from "@raycast/api";
+import { Form, ActionPanel, Action, showHUD, popToRoot } from "@raycast/api";
+import { runAppleScript } from "@raycast/utils";
+import path from "path";
 
-export default function Command() {
-  return <Detail markdown="# Hello World" />;
+interface CommandArguments {
+  URL?: string;
+}
+
+export default function Command(props: { arguments: CommandArguments }) {
+  async function saveWebloc(values: { folder: string[]; filename: string }) {
+    if (!values.folder || values.folder.length === 0) {
+      await showHUD("❌ No folder selected.");
+      return;
+    }
+
+    const fullPath = path.join(values.folder[0], `${values.filename}`);
+
+    const script = `
+      set savePath to "${fullPath}"
+      set fileRef to open for access POSIX file savePath with write permission
+      set eof of fileRef to 0
+      write "{ URL = \\"${props.arguments.URL ?? "https://example.com/"}\\"; }" to fileRef
+      close access fileRef
+    `;
+
+    try {
+      await runAppleScript(script);
+      await showHUD("✅ Webloc saved");
+    } catch (err) {
+      console.error(err);
+      await showHUD("❌ Error saving webloc");
+    }
+
+    await popToRoot();
+  }
+
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Save Webloc" onSubmit={saveWebloc} />
+        </ActionPanel>
+      }
+    >
+      <Form.FilePicker
+        id="folder"
+        title="Save Folder"
+        canChooseDirectories
+        canChooseFiles={false}
+        allowMultipleSelection={false}
+      />
+      <Form.TextField id="filename" title="Filename" placeholder="untitled.webloc" defaultValue="untitled.webloc" />
+    </Form>
+  );
 }
